0b71b3367688d519f72497ad294f27eee4169a16
Darcy Junior <darcy_junior@yahoo.com.br>
2021-11-06 13:28:13 -0300

    Criando a rota webhook

--- index.js
+++ index.js
@@ -9,30 +9,10 @@ app.get("/", (req, res) => {
   res.send("Olá chatbot");
 });
 
-app.get("/pergunta", (req, res) => {
-  const msg = req.query.pergunta;
-  res.send("Qual a sua pergunta?" + msg);
-});
+app.post("/webhook", (req, res) => {
+  console.log("cheguei no webhook");
 
-app.get("/mensagem/:tipo/:id", (req, res) => {
-  console.log(req.params.tipo);
-  res.send("resposta");
-});
-
-app.post("/pedido", (req, res) => {
-  console.log(req.body);
-  const produto = req.body.produto;
-  const qtd = req.body.quantidade;
-  const pgto = req.body.tipoPagamento;
-  const bebida = req.body.bebida;
-
-  const pedido = {
-    produto,
-    qtd,
-    pgto,
-    bebida,
-  };
-  res.json(pedido);
+  console.log("body", req.body.queryResult);
 });
 
 const porta = process.env.PORT || 3000;
@@ -41,3 +21,29 @@ const hostname = "127.0.0.1";
 app.listen(porta, () =>
   console.log(`Servidor inicializado em: http://${hostname}:${porta}`)
 );
+
+// app.get("/pergunta", (req, res) => {
+//   const msg = req.query.pergunta;
+//   res.send("Qual a sua pergunta?" + msg);
+// });
+
+// app.get("/mensagem/:tipo/:id", (req, res) => {
+//   console.log(req.params.tipo);
+//   res.send("resposta");
+// });
+
+// app.post("/pedido", (req, res) => {
+//   console.log(req.body);
+//   const produto = req.body.produto;
+//   const qtd = req.body.quantidade;
+//   const pgto = req.body.tipoPagamento;
+//   const bebida = req.body.bebida;
+
+//   const pedido = {
+//     produto,
+//     qtd,
+//     pgto,
+//     bebida,
+//   };
+//   res.json(pedido);
+// });
