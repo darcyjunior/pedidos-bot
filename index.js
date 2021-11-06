32fdaf35c9da3f37b769c2e5cced31b5c1db35c7
Darcy Junior <darcy_junior@yahoo.com.br>
2021-11-06 13:24:30 -0300

    Retirado o body-parser que está deprecated e atualizado no código e package.json

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Olá chatbot");
